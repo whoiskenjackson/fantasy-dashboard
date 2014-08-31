38124a764488c9ed2704b36a961e05c6c29bdf73
KennethJackson <kenneth@whoiskenjackson.com>
2014-08-31 18:47:37 -0400

    updating feed styling

--- dev/js/templates/compiled.js
+++ dev/js/templates/compiled.js
@@ -9,17 +9,17 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 function program1(depth0,data) {
   
   var buffer = "", stack1, helper;
-  buffer += "\n	<li data-index=\""
+  buffer += "<li data-index=\""
     + escapeExpression(((stack1 = (data == null || data === false ? data : data.index)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "\" data-parent=\"";
   if (helper = helpers.parent) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.parent); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + "\">\n		<div>\n			<h1>";
+    + "\">\n		<div>\n			<h3>";
   if (helper = helpers.title) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.title); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + "</h1>\n		</div>\n	</li>\n";
+    + "</h3>\n		</div>\n	</li>";
   return buffer;
   }
 
@@ -36,5 +36,5 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   
 
 
-  return "<div class=\"feed\"></div>\n\n<div class=\"modal\"></div>";
+  return "<div class=\"feed\" data-view=\"grid\"></div>\n\n<div class=\"modal\"></div>";
   });
